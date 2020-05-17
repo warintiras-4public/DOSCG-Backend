21c84a10f5e0defe7493b76e199e8907c310a271
warintiras <warintiras@gmail.com>
2020-05-17 21:24:01 +0700

    Move .env and edit controller

--- server/controllers/DOSCG.js
+++ server/controllers/DOSCG.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const redis = require('redis');
-
 const abc  = require('../models/DOSCG');
-require('dotenv').config('./.env');
+
+require('dotenv').config()
 
 const router = express.Router();
 const app = express();
@@ -18,30 +18,38 @@ router.get('/', (req, res) => {
     res.send('Hello SCG!');
 });
 
-router.get('/direction', cache, getDirection);
-router.get('/abc', findAandC);
+router.get('/direction', cacheDirection, getDirection);
+router.get('/abc', cacheAbc, findAandC);
 router.get('/xyz', findXYZ);
 
 // Find a and c
 function findAandC(req, res, next) {
-    // const a = 21;
-    // const ab = 23;
-    // const ac = -21;
-    const a = abc.find(x => x.id === "a").value; 
-    const ab = abc.find(x => x.id === "ab").value;
-    const ac = abc.find(x => x.id === "ac").value;
+    try {
+        console.log("Fetching abc ...");
+  
+        // a = 21, ab = 23, ac = -21;
+        const a = abc.find(x => x.id === "a").value; 
+        const ab = abc.find(x => x.id === "ab").value;
+        const ac = abc.find(x => x.id === "ac").value;
 
-    // const b = 23-21;
-    const b = ab-a;
+        // b = 23-21;
+        const b = ab-a;
 
-    // const c = -21-21
-    const c = ac-a
+        // c = -21-21
+        const c = ac-a
 
-    const bc = [
-        { id: 1, b: b, c: c}
-    ];
+        const bc = { b: b, c: c};
 
-    res.send(bc);
+        const stringifyData = JSON.stringify(bc);
+
+        // Set data to Redis
+        client.setex(a, 3600, stringifyData);
+
+        res.send(bc);
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    }
 }
 
 // Find x, y, and z
@@ -71,10 +79,26 @@ async function getDirection(req, res, next) {
 }
 
 // Cache middleware
-function cache(req, res, next) {
+function cacheDirection(req, res, next) {
     const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
     client.get(GOOGLE_API_KEY, (err, data) => {
+
+        if (err) throw err;
+
+        if(data!== null) {
+            res.send(JSON.parse(data));
+        } else {
+            next();
+
+        }
+    })
+}
+
+function cacheAbc(req, res, next) {
+    const a = abc.find(x => x.id === "a").value; 
+
+    client.get(a, (err, data) => {
 
         if (err) throw err;
 
